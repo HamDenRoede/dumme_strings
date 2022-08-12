2a04f2b2235f4df11cc3f51eb6ccf554f65dad6d
HamDenRoede <cph-ji63@cphbusiness.dk>
2022-08-12 09:57:23 +0200

    working substring

--- Program.cs
+++ Program.cs
@@ -6,22 +6,26 @@ namespace more_variable_manipulation
     {
         static void Main(string[] args)
         {
+
             nameLength();
         }
 
         static void nameLength()
         {
+
             String navn = "Jens Ole Hansen";
             int antalTegn = navn.Length;
             //Console.WriteLine("Der er {0} tegn i variabler", antalTegn);
             //Console.WriteLine("Formatet er {0}", navn.Substring(0,4));
 
-            for (int pos = 0; navn.Substring(pos, 1) != " "; pos = +1)
-            {
-                Console.WriteLine(navn.Substring(pos, 1));
-            }
            
-            Console.Write("Done!"); Console.ReadKey();
+            
+            for (int pos = 0; navn.Substring(pos, 1) != " "; pos = pos +1)
+            {
+                Console.Write(navn.Substring(pos, 1));
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Done!"); Console.ReadKey();
         }
     }
 }
